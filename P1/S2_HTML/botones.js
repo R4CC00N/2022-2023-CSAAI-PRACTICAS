7a65562f49e2a5a76f572c2dae7ddc8fa9067619
pecamu <pe.camuendo.2020@alumnos.urjc.es>
2023-02-23 08:51:54 +0000

    JS

--- P1/S2_HTML/botones.js
+++ P1/S2_HTML/botones.js
@@ -1,8 +1,8 @@
 console.log("Ejecutnado JS...");
 
 //-- Obtener el párrafo
-const btn1 = document.getElementById('bnt1');
-const btn2 = document.getElementById('bnt2');
+const btn1 = document.getElementById('btn1');
+const btn2 = document.getElementById('btn2');
 
 //-- Funcion de retrollamada de la pulsación del párrafo
 btn1.onclick = () => {
