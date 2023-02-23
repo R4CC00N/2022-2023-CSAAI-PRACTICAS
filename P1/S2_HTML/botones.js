8331bd6d9c01934a12a7526a89442d2d5301cefd
pecamu <pe.camuendo.2020@alumnos.urjc.es>
2023-02-23 08:51:06 +0000

    JS

--- P1/S2_HTML/botones.js
+++ P1/S2_HTML/botones.js
@@ -6,30 +6,11 @@ const btn2 = document.getElementById('bnt2');
 
 //-- Funcion de retrollamada de la pulsación del párrafo
 btn1.onclick = () => {
-    console.log("Click!");
+    console.log("Click! BTN1");
 
-    //-- Cambiar el color del fondo...
-    //-- Si no tenía color asignado ponemos amarillo
-    if (btn1.backgroundColor == "") {
-        btn1.backgroundColor = "yellow";
-    }
-    //-- Si ya tiene color se lo quitamos
-    else {
-        btn1.backgroundColor = "";
-    }
 
 }
 btn2.onclick = () => {
-    console.log("Click!");
-
-    //-- Cambiar el color del fondo...
-    //-- Si no tenía color asignado ponemos amarillo
-    if (btn1.backgroundColor == "") {
-        btn1.backgroundColor = "red";
-    }
-    //-- Si ya tiene color se lo quitamos
-    else {
-        btn1.backgroundColor = "";
-    }
+    console.log("Click! BTN2");
 
 }
